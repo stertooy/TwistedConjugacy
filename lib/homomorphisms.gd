a90050fce357124449168f0eedf1f74fcadb4c9d
stertooy <5571903+stertooy@users.noreply.github.com>
2025-09-11 20:20:16 +0200

    Create homomorphisms.gd

---
+++ lib/homomorphisms.gd
@@ -0,0 +1,15 @@
+DeclareGlobalFunction( "RepresentativesHomomorphismClasses" );
+DeclareOperation(
+    "RepresentativesHomomorphismClassesOp",
+    [ IsGroup, IsGroup ]
+);
+DeclareGlobalFunction( "RepresentativesEndomorphismClasses" );
+DeclareOperation(
+    "RepresentativesEndomorphismClassesOp",
+    [ IsGroup ]
+);
+DeclareGlobalFunction( "RepresentativesAutomorphismClasses" );
+DeclareOperation(
+    "RepresentativesAutomorphismClassesOp",
+    [ IsGroup ]
+);
