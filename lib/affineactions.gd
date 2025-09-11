fedb478ca3c581c0d4a12c1498b57c0eadd7ffd2
stertooy <5571903+stertooy@users.noreply.github.com>
2025-09-11 20:30:39 +0200

    Create affineactions.gd

---
+++ lib/affineactions.gd
@@ -0,0 +1,11 @@
+DeclareGlobalFunction( "OrbitAffineAction" );
+
+DeclareGlobalFunction( "OrbitsAffineAction" );
+DeclareGlobalFunction( "NrOrbitsAffineAction" );
+
+DeclareGlobalFunction( "StabilizerAffineAction" );
+DeclareSynonym( "StabiliserAffineAction", StabilizerAffineAction );
+
+DeclareGlobalFunction( "RepresentativeAffineAction" );
+
+DeclareGlobalFunction( "AffineActionByGroupDerivation" );
