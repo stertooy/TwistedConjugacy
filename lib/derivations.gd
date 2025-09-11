a5ffeccefda7e6d2bfb6598c6e360880ceb75acd
stertooy <5571903+stertooy@users.noreply.github.com>
2025-09-11 20:25:40 +0200

    Create derivations.gd

---
+++ lib/derivations.gd
@@ -0,0 +1,17 @@
+DeclareCategory( "IsGroupDerivation", IsSPGeneralMapping );
+InstallTrueMethod( IsMapping, IsGroupDerivation );
+InstallTrueMethod( RespectsOne, IsGroupDerivation );
+
+DeclareAttribute( "GroupDerivationInfo", IsGroupDerivation );
+DeclareAttribute( "KernelOfGroupDerivation", IsGroupDerivation );
+
+DeclareGlobalFunction( "GroupDerivationByImagesNC" );
+DeclareGlobalFunction( "GroupDerivationByImages" );
+DeclareRepresentation( "IsGroupDerivationByImages", IsAttributeStoringRep );
+InstallTrueMethod( IsGroupDerivation, IsGroupDerivationByImages );
+
+DeclareGlobalFunction( "GroupDerivationByFunction" );
+DeclareRepresentation( "IsGroupDerivationByFunction", IsAttributeStoringRep );
+InstallTrueMethod( IsGroupDerivation, IsGroupDerivationByFunction );
+
+DeclareRepresentation( "IsGroupDerivationImageRep", IsExternalOrbit );
