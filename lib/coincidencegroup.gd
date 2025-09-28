e2f73906fb565f8d9f6e5de496a308cbc113cda7
stertooy <5571903+stertooy@users.noreply.github.com>
2025-09-28 14:41:56 +0200

    Update coincidencegroup.gd

--- lib/coincidencegroup.gd
+++ lib/coincidencegroup.gd
@@ -1,7 +1,6 @@
-DeclareGlobalFunction( "FixedPointGroup" );
-DeclareGlobalFunction( "CoincidenceGroup" );
+DeclareGlobalName( "FixedPointGroup" );
+DeclareGlobalName( "CoincidenceGroup" );
 DeclareOperation(
     "CoincidenceGroup2",
     [ IsGroupHomomorphism, IsGroupHomomorphism ]
 );
-DeclareGlobalFunction( "TWC_InducedCoincidenceGroup" );
