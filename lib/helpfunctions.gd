c76378d09d56283618e15ee32ed1681067603af3
stertooy <5571903+stertooy@users.noreply.github.com>
2025-09-28 12:59:24 +0200

    Update helpfunctions.gd

--- lib/helpfunctions.gd
+++ lib/helpfunctions.gd
@@ -1,5 +1,5 @@
 DeclareProperty( "IsNilpotentByAbelian", IsGroup );
 InstallTrueMethod( IsNilpotentByAbelian, IsNilpotentGroup );
 
-DeclareGlobalFunction( "TWC_IntersectionOfKernels" );
-DeclareGlobalFunction( "TWC_IntersectionOfPreImages" );
+DeclareGlobalName( "TWC_IntersectionOfKernels" );
+DeclareGlobalName( "TWC_IntersectionOfPreImages" );
