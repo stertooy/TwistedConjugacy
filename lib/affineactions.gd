c8fce0f02013cfaa58b9fde454ed32eea3a0ddb2
stertooy <5571903+stertooy@users.noreply.github.com>
2026-03-22 17:20:07 +0100

    Update affineactions.gd

--- lib/affineactions.gd
+++ lib/affineactions.gd
@@ -148,10 +148,10 @@ DeclareGlobalName( "StabilizerOfExternalSet" );
 #! @GroupTitle List
 #! @Returns a list containing the elements of <A>orb</A>.
 #! @Description
-###############################################################################
 #! If <A>orb</A> is infinite, this will run forever or cause an error. It is
 #! recommended to first test the finiteness of <A>orb</A> using
-#! <Ref Prop="IsFinite" Label="for an orbit of an affine action" Style="Number"/>.
+#! <Ref Prop="IsFinite" Label="for an orbit of an affine action"
+#! Style="Number"/>.
 #! @Label of an orbit of an affine action
 #! @ItemType Func
 #! @Arguments orb
