cfbd71aa7212f2b44bf6f4d586e238dbc48a8797
stertooy <5571903+stertooy@users.noreply.github.com>
2026-05-06 12:11:59 +0200

    Update reidemeisterspectrum.gd

--- lib/reidemeisterspectrum.gd
+++ lib/reidemeisterspectrum.gd
@@ -1,7 +1,7 @@
 #! @Returns the Reidemeister spectrum of <A>G</A>.
 #! @ChapterInfo reidemeisternumbers, reidemeisterspectra
 #! @Description
-#! If $G$ is abelian, this function relies on the results from
+#! If <A>G</A> is abelian, this function relies on the results from
 #! <Cite Key='send23-a'/>.
 #! Otherwise, it relies on <Cite Key='ree59-a' Where='Thm. 1'/>.
 #! @Arguments G
@@ -12,8 +12,9 @@ DeclareOperation( "ReidemeisterSpectrumOp", [ IsGroup ] );
 #! @Returns the extended Reidemeister spectrum of <A>G</A>.
 #! @ChapterInfo reidemeisternumbers, reidemeisterspectra
 #! @Description
-#! If $G$ is abelian, this is just the set of all divisors of the order of
-#! <A>G</A>.
+#! If <A>G</A> is simple, this is the union of its Reidemeister spectrum with
+#! the element $1$.
+#! If <A>G</A> is abelian, this is just the set of all divisors its order.
 #! Otherwise, this function relies on <Cite Key='ree59-a' Where='Thm. 1'/>.
 #! @Arguments G
 DeclareGlobalFunction( "ExtendedReidemeisterSpectrum" );
